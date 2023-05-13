46bd88b5541e6afaf06eb3572cd9c9e2a9e1f985
AlexNi245 <alex.plutta@googlemail.com>
2023-05-13 22:02:16 +0200

    add Gasestimation script

---
+++ test/GasEstimations.test.ts
@@ -0,0 +1,81 @@
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { ethers, ethers as hreEthers } from "hardhat";
+
+describe("Gas Estimations", () => {
+    let deployer: SignerWithAddress;
+
+    beforeEach(async () => {
+        [deployer] = await ethers.getSigners();
+    });
+    describe("BedrockProofVerifier", () => {
+        it("Deployment", async () => {
+            const BedrockProofVerifierFactory = await hreEthers.getContractFactory("BedrockProofVerifier");
+
+            const deployTx = BedrockProofVerifierFactory.getDeployTransaction(ethers.constants.AddressZero);
+       
+            const deployTxResponse = await deployer.sendTransaction(deployTx);
+
+            const deployTxReceipt = await deployTxResponse.wait();
+
+            const gasUsed = deployTxReceipt.gasUsed;
+
+            console.log(`Optimism Proof Verifier deployment took ${gasUsed} Gas`);
+
+            console.log(
+                "Based on 50 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("50", "gwei")))
+            );
+            console.log(
+                "Based on 60 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("60", "gwei")))
+            );
+            console.log(
+                "Based on 70 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("70", "gwei")))
+            );
+            console.log(
+                "Based on 80 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("80", "gwei")))
+            );
+            console.log(
+                "Based on 90 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("90", "gwei")))
+            );
+            console.log(
+                "Based on 100 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("100", "gwei")))
+            );
+        });
+        it("Deployment", async () => {
+            const BedrockProofVerifierFactory = await hreEthers.getContractFactory("OptimismResolver");
+
+            const deployTx = BedrockProofVerifierFactory.getDeployTransaction(
+                "http://localhost:8080/{sender}/{data}",
+                deployer.address,
+                ethers.constants.AddressZero,
+                ethers.constants.AddressZero
+            );
+
+            const deployTxResponse = await deployer.sendTransaction(deployTx);
+
+            const deployTxReceipt = await deployTxResponse.wait();
+
+            const gasUsed = deployTxReceipt.gasUsed;
+
+            console.log(`OptimismResolver deployment took ${gasUsed} Gas`);
+
+            console.log(
+                "Based on 50 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("50", "gwei")))
+            );
+            console.log(
+                "Based on 60 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("60", "gwei")))
+            );
+            console.log(
+                "Based on 70 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("70", "gwei")))
+            );
+            console.log(
+                "Based on 80 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("80", "gwei")))
+            );
+            console.log(
+                "Based on 90 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("90", "gwei")))
+            );
+            console.log(
+                "Based on 100 GWEI it would've cost : " + ethers.utils.formatEther(gasUsed.mul(ethers.utils.parseUnits("100", "gwei")))
+            );
+        });
+    });
+});
