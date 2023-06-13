0e93cbd465a27c5cd5f1fafde3a622756ccbb4ef
AlexNi245 <alex.plutta@googlemail.com>
2023-06-13 16:07:27 +0200

    add text resolver supporting context

--- contracts/l2/L2PublicResolver.sol
+++ contracts/l2/L2PublicResolver.sol
@@ -3,11 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@ensdomains/ens-contracts/contracts/resolvers/Multicallable.sol";
 import {AddrResolver} from "./profiles/addr/AddrResolver.sol";
+import {TextResolver} from "./profiles/text/TextResolver.sol";
 
 import {LibOwnedENSNode} from "../lib/LibOwnedENSNode.sol";
 
-contract L2PublicResolver is Multicallable, AddrResolver {
-    function supportsInterface(bytes4 interfaceID) public view override(Multicallable, AddrResolver) returns (bool) {
+contract L2PublicResolver is Multicallable, AddrResolver, TextResolver {
+    function supportsInterface(bytes4 interfaceID) public view override(Multicallable, AddrResolver, TextResolver) returns (bool) {
         return super.supportsInterface(interfaceID);
     }
 }
---
+++ contracts/l2/profiles/text/TextResolver.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import "../ResolverBase.sol";
+import "./ITextResolver.sol";
+
+abstract contract TextResolver is ITextResolver, ResolverBase {
+    mapping(uint64 => mapping(bytes => mapping(bytes32 => mapping(string => string)))) versionable_texts;
+
+    /**
+     * Sets the text data associated with an ENS node and key.
+     * May only be called by the owner of that node in the ENS registry.
+     * @param node The node to update.
+     * @param key The key to set.
+     * @param value The text data value to set.
+     */
+    function setText(bytes32 node, string calldata key, string calldata value) external virtual {
+        bytes memory context = abi.encodePacked(msg.sender);
+        versionable_texts[recordVersions[context][node]][context][node][key] = value;
+        emit TextChanged(node, key, key, value);
+    }
+
+    /**
+     * Returns the text data associated with an ENS node and key.
+     * @param node The ENS node to query.
+     * @param key The text data key to query.
+     * @return The associated text data.
+     */
+    function text(bytes calldata context, bytes32 node, string calldata key) external view virtual override returns (string memory) {
+        return versionable_texts[recordVersions[context][node]][context][node][key];
+    }
+
+    function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
+        return interfaceID == type(ITextResolver).interfaceId || super.supportsInterface(interfaceID);
+    }
+}
