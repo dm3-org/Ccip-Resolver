3e98c0bbe7cd7edf0d965b2430f7232c69665c03
AlexNi245 <alex.plutta@googlemail.com>
2023-06-13 15:45:34 +0200

    add addr resolver with context

---
+++ contracts/l2/profiles/ResolverBase.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+
+interface IVersionableResolver {
+    event VersionChanged(bytes32 indexed node, uint64 newVersion);
+
+    function recordVersions(bytes calldata context, bytes32 node) external view returns (uint64);
+}
+
+abstract contract ResolverBase is ERC165, IVersionableResolver {
+    mapping(bytes => mapping(bytes32 => uint64)) public recordVersions;
+
+    /**
+     * Increments the record version associated with an ENS node.
+     * May only be called by the owner of that node in the ENS registry.
+     * @param node The node to update.
+     */
+    function clearRecords(bytes32 node) public virtual {
+        bytes memory context = abi.encode(msg.sender);
+        recordVersions[context][node]++;
+        // emit VersionChanged(node, recordVersions[node]);
+    }
+
+    function supportsInterface(bytes4 interfaceID) public view virtual override returns (bool) {
+        return interfaceID == type(IVersionableResolver).interfaceId || super.supportsInterface(interfaceID);
+    }
+}
---
+++ contracts/l2/profiles/addr/IAddressResolver.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4;
+
+/**
+ * Interface for the new (multicoin) addr function.
+ */
+interface IAddressResolver {
+    event AddressChanged(bytes32 indexed node, uint256 coinType, bytes newAddress);
+
+    function addr(bytes calldata context, bytes32 node, uint256 coinType) external view returns (bytes memory);
+}
