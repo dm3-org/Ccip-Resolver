40d4ffe3f5a7b5243b8fc732edfcf685b552dca6
AlexNi245 <alex.plutta@googlemail.com>
2023-06-14 10:12:07 +0200

    get rid OwnedNode Lib + Test

--- contracts/lib/LibOwnedENSNode.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-library LibOwnedENSNode {
-    function getOwnedENSNode(bytes32 node, address owner) internal pure returns (bytes32) {
-        return keccak256(abi.encode(node, owner));
-    }
-}
