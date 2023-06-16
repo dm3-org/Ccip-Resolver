063df9c1596f4e31ef64f12a2adca06b6a42832a
AlexNi245 <alex.plutta@googlemail.com>
2023-06-16 10:54:58 +0200

    add function to change l2resolver

--- contracts/l1/verifier/optimism-bedrock/BedrockCcipVerifier.sol
+++ contracts/l1/verifier/optimism-bedrock/BedrockCcipVerifier.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.15;
 import {CcipResponseVerifier} from "../CcipResponseVerifier.sol";
 import {IBedrockProofVerifier} from "./IBedrockProofVerifier.sol";
 
+//TODO use OZ Ownable
 contract BedrockCcipVerifier is CcipResponseVerifier {
     IBedrockProofVerifier public bedrockProofVerifier;
     address public l2Resolver;
@@ -11,6 +12,11 @@ contract BedrockCcipVerifier is CcipResponseVerifier {
     constructor(IBedrockProofVerifier _bedrockProofVerifier, address _l2Resolver) {
         bedrockProofVerifier = _bedrockProofVerifier;
         l2Resolver = _l2Resolver;
+    }
+
+    //TODO add onlyOwner
+    function setL2Resolver(address l2Resolver) external {
+        l2Resolver = l2Resolver;
     }
 
     function resolveWithProof(bytes calldata response, bytes calldata extraData) external view override returns (bytes memory) {
