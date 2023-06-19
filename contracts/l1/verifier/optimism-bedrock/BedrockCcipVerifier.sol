062a28beac1fff576d6d6d9e3f4677375c89a61c
AlexNi245 <alex.plutta@googlemail.com>
2023-06-19 15:06:59 +0200

    make l2Resolver and bedrockProofVerifier immutable in BedrockCcipVerifier

--- contracts/l1/verifier/optimism-bedrock/BedrockCcipVerifier.sol
+++ contracts/l1/verifier/optimism-bedrock/BedrockCcipVerifier.sol
@@ -4,26 +4,17 @@ pragma solidity ^0.8.15;
 import {CcipResponseVerifier} from "../CcipResponseVerifier.sol";
 import {IBedrockProofVerifier} from "./IBedrockProofVerifier.sol";
 
-//TODO use OZ Ownable
 contract BedrockCcipVerifier is CcipResponseVerifier {
-    IBedrockProofVerifier public bedrockProofVerifier;
-    address public l2Resolver;
+    IBedrockProofVerifier public immutable bedrockProofVerifier;
+    address public immutable l2Resolver;
 
     constructor(IBedrockProofVerifier _bedrockProofVerifier, address _l2Resolver) {
         bedrockProofVerifier = _bedrockProofVerifier;
         l2Resolver = _l2Resolver;
     }
 
-    //TODO add onlyOwner
-    function setL2Resolver(address l2Resolver) external {
-        l2Resolver = l2Resolver;
-    }
-
     function resolveWithProof(bytes calldata response, bytes calldata extraData) external view override returns (bytes memory) {
-        (string memory result, IBedrockProofVerifier.BedrockStateProof memory proof) = abi.decode(
-            response,
-            (string, IBedrockProofVerifier.BedrockStateProof)
-        );
+        (, IBedrockProofVerifier.BedrockStateProof memory proof) = abi.decode(response, (string, IBedrockProofVerifier.BedrockStateProof));
 
         require(proof.target == l2Resolver, "proof target does not match resolver");
         return bedrockProofVerifier.getProofValue(proof);
