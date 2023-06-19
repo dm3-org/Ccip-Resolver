68b17639a09c2f6002f529012b4871f0416af694
AlexNi245 <alex.plutta@googlemail.com>
2023-06-19 13:50:46 +0200

    get rid of mock namewrapper file

--- test/contracts/l1/OptimismResolver/mockNamewrapper.ts
+++
@@ -1,8 +0,0 @@
-import { FakeContract, smock } from "@defi-wonderland/smock";
-import { INameWrapper } from "typechain";
-
-export const mockEnsRegistry = async (node, owner) => {
-    const nameWrapper = (await smock.fake("@ensdomains/ens-contracts/contracts/wrapper/NameWrapper.sol")) as FakeContract<INameWrapper>;
-    nameWrapper.ownerOf.whenCalledWith(node).returns(owner);
-    return nameWrapper;
-};
