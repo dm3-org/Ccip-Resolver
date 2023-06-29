2b7f3717e1a27ec0e4d6c905bec5b77626bf7758
AlexNi245 <alex.plutta@googlemail.com>
2023-06-29 11:26:29 +0200

    ProofServiceTest is now ENS agnostic

--- test/e2e/ProofService.test.ts
+++ test/e2e/ProofService.test.ts
@@ -1,24 +1,19 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
-import { BedrockProofVerifier, L2PublicResolver, L2PublicResolver__factory } from "typechain";
-import { getPublicResolverAddress } from "../../gateway/constants";
-import { EnsResolverService } from "../../gateway/service/ens/EnsService";
+import { BedrockProofVerifier, ProofServiceTestContract__factory, } from "typechain";
+
 import { ProofService, StorageLayout } from "../../gateway/service/proof/ProofService";
-import { dnsEncode } from "ethers/lib/utils";
 import { BigNumber } from "ethers";
 
-const PUBLIC_RESOLVER_ADDRESS = getPublicResolverAddress();
 
-const PROOF_SERVICE_TEST_CONTRACT = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const PROOF_SERVICE_TEST_CONTRACT = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 describe("ProofServiceTest", () => {
     let BedrockProofVerifier: BedrockProofVerifier;
-    let publicResolver: L2PublicResolver;
 
     const l1Provider = new ethers.providers.StaticJsonRpcProvider("http://localhost:8545");
     const l2Provider = new ethers.providers.StaticJsonRpcProvider("http://localhost:9545");
 
-    //0x8111DfD23B99233a7ae871b7c09cCF0722847d89
-    const alice = new ethers.Wallet("0xfd9f3842a10eb01ccf3109d4bd1c4b165721bf8c26db5db7570c146f9fad6014");
 
     beforeEach(async () => {
         //See github.com/ethereum-optimism/optimism/op-bindings/predeploys/dev_addresses.go
@@ -26,85 +21,49 @@ describe("ProofServiceTest", () => {
         BedrockProofVerifier = (await BedrockProofVerifierFactory.deploy(
             "0x6900000000000000000000000000000000000000"
         )) as BedrockProofVerifier;
-
-        const factory = (await ethers.getContractFactory("L2PublicResolver")) as L2PublicResolver__factory;
-        publicResolver = await factory.attach(PUBLIC_RESOLVER_ADDRESS).connect(l2Provider);
     });
 
-    it("bytes32 slot", async () => {
+    it("bool slot", async () => {
         const proofService = new ProofService(l1Provider, l2Provider);
         const { proof } = await proofService.createProof(PROOF_SERVICE_TEST_CONTRACT, ethers.constants.HashZero, StorageLayout.FIXED);
 
         const responseBytes = await BedrockProofVerifier.getProofValue(proof);
-        console.log(responseBytes);
+        expect(responseBytes).to.equal("0x01");
+    })
+    it("bytes32 slot", async () => {
+        const proofService = new ProofService(l1Provider, l2Provider);
+        const { proof } = await proofService.createProof(PROOF_SERVICE_TEST_CONTRACT, "0x0000000000000000000000000000000000000000000000000000000000000001", StorageLayout.FIXED);
+
+        const responseBytes = await BedrockProofVerifier.getProofValue(proof);
         expect(responseBytes).to.equal(ethers.utils.namehash("alice.eth"));
     })
 
-    it("empy slot", async () => {
+    it("address slot", async () => {
         const proofService = new ProofService(l1Provider, l2Provider);
-
-        const node = ethers.utils.namehash("alice1234.eth");
-        const recordName = "empy-slot";
-
-
-        const slot = EnsResolverService.getStorageSlotForText(2, 0, alice.address, node, recordName);
-        const { proof } = await proofService.createProof(PUBLIC_RESOLVER_ADDRESS, slot);
-
-        expect(proof.length).to.be.equal(0);
+        const { proof } = await proofService.createProof(PROOF_SERVICE_TEST_CONTRACT, "0x0000000000000000000000000000000000000000000000000000000000000002", StorageLayout.FIXED);
 
         const responseBytes = await BedrockProofVerifier.getProofValue(proof);
-        const responseString = Buffer.from(responseBytes.slice(2), "hex").toString();
-
-        expect(responseString).to.equal("");
-    });
-    it("sinlge slot", async () => {
+        expect(ethers.utils.getAddress(responseBytes)).to.equal("0x8111DfD23B99233a7ae871b7c09cCF0722847d89");
+    })
+    it("uint slot", async () => {
         const proofService = new ProofService(l1Provider, l2Provider);
-
-        const recordName = "foo";
-
-        const node = ethers.utils.namehash("alice.eth")
-
-        const slot = EnsResolverService.getStorageSlotForText(2, 0, alice.address, node, recordName);
-        const { proof } = await proofService.createProof(PUBLIC_RESOLVER_ADDRESS, slot);
-
-        expect(proof.length).to.be.equal(3);
+        const { proof } = await proofService.createProof(PROOF_SERVICE_TEST_CONTRACT, "0x0000000000000000000000000000000000000000000000000000000000000003", StorageLayout.FIXED);
 
         const responseBytes = await BedrockProofVerifier.getProofValue(proof);
-        const responseString = Buffer.from(responseBytes.slice(2), "hex").toString();
-
-        expect(responseString).to.eql("bar");
-    });
-    it("sinlge slot 31 bytes long", async () => {
+        expect(BigNumber.from(responseBytes)).to.equal("123");
+    })
+    it("bytes slot", async () => {
         const proofService = new ProofService(l1Provider, l2Provider);
-
-        const recordName = "my-slot";
-
-        const node = ethers.utils.namehash("alice.eth")
-
-        const slot = EnsResolverService.getStorageSlotForText(2, 0, alice.address, node, recordName);
-        const { proof } = await proofService.createProof(PUBLIC_RESOLVER_ADDRESS, slot);
+        const { proof } = await proofService.createProof(PROOF_SERVICE_TEST_CONTRACT, "0x0000000000000000000000000000000000000000000000000000000000000004", StorageLayout.DYNAMIC);
 
         const responseBytes = await BedrockProofVerifier.getProofValue(proof);
-        const responseString = Buffer.from(responseBytes.slice(2), "hex").toString();
-
-        expect(responseString).to.eql("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-    });
-    it("multislot slot", async () => {
+        expect(responseBytes).to.equal(ethers.utils.namehash("alice.eth"));
+    })
+    it("string slot", async () => {
         const proofService = new ProofService(l1Provider, l2Provider);
+        const { proof } = await proofService.createProof(PROOF_SERVICE_TEST_CONTRACT, "0x0000000000000000000000000000000000000000000000000000000000000005", StorageLayout.DYNAMIC);
 
-        const node = ethers.utils.namehash("alice.eth");
-        const recordName = "network.dm3.eth";
-
-        const slot = EnsResolverService.getStorageSlotForText(2, 0, alice.address, node, recordName);
-        const { proof } = await proofService.createProof(PUBLIC_RESOLVER_ADDRESS, slot);
         const responseBytes = await BedrockProofVerifier.getProofValue(proof);
-        const profile = {
-            publicSigningKey: "0ekgI3CBw2iXNXudRdBQHiOaMpG9bvq9Jse26dButug=",
-            publicEncryptionKey: "Vrd/eTAk/jZb/w5L408yDjOO5upNFDGdt0lyWRjfBEk=",
-            deliveryServices: ["foo.dm3"],
-        };
-        const responseString = Buffer.from(responseBytes.slice(2), "hex").toString();
-
-        expect(responseString).to.eql(JSON.stringify(profile));
-    });
+        expect(Buffer.from(responseBytes.slice(2), "hex").toString()).to.equal("Hello from Alice");
+    })
 });
--- test/e2e/setupEnvironment.ts
+++ test/e2e/setupEnvironment.ts
@@ -1,7 +1,5 @@
 import { ethers } from "hardhat";
-import { dnsWireFormat } from "../helper/encodednsWireFormat";
-import { L2PublicResolver, L2PublicResolver__factory, ProofServiceTestContract, ProofServiceTestContract__factory } from "typechain";
-import { dnsEncode, keccak256, toUtf8Bytes } from "ethers/lib/utils";
+import { ProofServiceTestContract, ProofServiceTestContract__factory } from "typechain";
 /**
  * This script is used to setup the environment for the e2e tests.
  * It asumes that you've set up the local development environment for OP bedrock
@@ -20,14 +18,9 @@ const l1Provider = new ethers.providers.StaticJsonRpcProvider("http://localhost:
 const l2Provider = new ethers.providers.StaticJsonRpcProvider("http://localhost:9545");
 
 const setupEnvironment = async () => {
-    //The resolver that is linked in the OptimismResolver contract
-    let l2PublicResolver: L2PublicResolver;
-    //Another resolver contract not related to the OptimismResolver contract
-    let foreignResolver: L2PublicResolver
-
     //Test proof logic
-
     let proofServiceTestContract: ProofServiceTestContract;
+    let foreignProofServiceTestContract: ProofServiceTestContract;
 
     const l2Whale = whale.connect(l2Provider);
 
@@ -37,195 +30,45 @@ const setupEnvironment = async () => {
         return;
     }
 
-    const factory = (await ethers.getContractFactory("L2PublicResolver")) as L2PublicResolver__factory;
-    l2PublicResolver = await factory.connect(l2Whale).deploy();
-    await l2PublicResolver.deployed();
-    console.log(`L2 Resolver deployed at ${l2PublicResolver.address}`);
 
-    foreignResolver = await factory.connect(l2Whale).deploy();
-    await foreignResolver.deployed();
-    console.log(`L2 Foreign resolver deployed at ${foreignResolver.address}`);
 
 
     const proofServiceTestContractFactory = (await ethers.getContractFactory("ProofServiceTestContract")) as ProofServiceTestContract__factory;
+
     proofServiceTestContract = await proofServiceTestContractFactory.connect(l2Whale).deploy();
+    foreignProofServiceTestContract = await proofServiceTestContractFactory.connect(l2Whale).deploy();
+
     console.log(`ProofServiceTestContract deployed at ${proofServiceTestContract.address}`);
+    console.log(`ForeignProofServiceTestContract deployed at ${foreignProofServiceTestContract.address}`);
 
-    //Fund alice account
-    const fundTx = await l2Whale.sendTransaction({
-        to: alice.address,
-        value: ethers.utils.parseEther("100"),
-    });
-
-    await fundTx.wait();
-    console.log(`${alice.address} funded with ${await l2Provider.getBalance(alice.address)}`);
-    //Fund bob account
-    const fundTxbob = await l2Whale.sendTransaction({
-        to: bob.address,
-        value: ethers.utils.parseEther("100"),
-    });
-
-    await fundTxbob.wait();
-    console.log(`${bob.address} funded with ${await l2Provider.getBalance(bob.address)}`);
-
-    //Create data on L2 that later be used for the tests
-    const prepareTestPrimitiveSlot = async () => {
-        //Prepare test single slot
-        const name = dnsEncode("bob.eth");
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).clearRecords(name);
-        const rec = await tx.wait();
-        console.log(rec.events);
+    /*     //Fund alice account
+        const fundTx = await l2Whale.sendTransaction({
+            to: alice.address,
+            value: ethers.utils.parseEther("100"),
+        });
+        await fundTx.wait();
+    
+        console.log(`${alice.address} funded with ${await l2Provider.getBalance(alice.address)}`);
+        //Fund bob account
+        const fundTxbob = await l2Whale.sendTransaction({
+            to: bob.address,
+            value: ethers.utils.parseEther("100"),
+        });
+    
+        await fundTxbob.wait();
+        console.log(`${bob.address} funded with ${await l2Provider.getBalance(bob.address)}`); */
 
 
-    };
-    const prepareTestSingleSlot = async () => {
-        //Prepare test single slot
-        const name = dnsEncode("alice.eth");
-
-        const recordName = "foo";
-        const value = "bar";
-        await l2PublicResolver.connect(alice.connect(l2Provider)).setText(name, recordName, value);
-
-    };
-
-    //Prepare test 31 byte
-    const prepareTest31yte = async () => {
-        const name = dnsEncode("alice.eth");
-        const recordName = "my-slot";
-        const value = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-
-        await l2PublicResolver.connect(alice.connect(l2Provider)).setText(name, recordName, value);
-    };
-
-    //Prepare test multiple slots
-    const prepeTestMultipleSlots = async () => {
-        const name = dnsEncode("alice.eth");
-        const recordName = "network.dm3.eth";
-
-        const profile = {
-            publicSigningKey: "0ekgI3CBw2iXNXudRdBQHiOaMpG9bvq9Jse26dButug=",
-            publicEncryptionKey: "Vrd/eTAk/jZb/w5L408yDjOO5upNFDGdt0lyWRjfBEk=",
-            deliveryServices: ["foo.dm3"],
-        };
-
-        await l2PublicResolver.connect(alice.connect(l2Provider)).setText(name, recordName, JSON.stringify(profile));
-    };
-
-    //Prepare setAddr
-    const prepareSetAddr = async () => {
-        const name = dnsEncode("alice.eth");
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider))["setAddr(bytes,address)"](name, alice.
-            address);
-
-    };
-    const prepareSetAbi = async () => {
-        const name = dnsEncode("alice.eth");
-        const abi = l2PublicResolver.interface.format(ethers.utils.FormatTypes.json);
-
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).setABI(name, 1, ethers.utils.toUtf8Bytes(abi.toString()));
-        const rec = await tx.wait();
-    }
-    const prepareSetContentHash = async () => {
-        const name = dnsEncode("alice.eth");
-
-
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).setContenthash(name, "0xe3010170122029f2d17be6139079dc48696d1f582a8530eb9805b561eda517e22a892c7e3f1f")
-        const rec = await tx.wait();
-    }
-    const prepareSetName = async () => {
-        const nodeName = dnsEncode("alice.eth");
-        const name = "alice";
-
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).setName(nodeName, name)
-        const rec = await tx.wait();
-
-    }
-    const prepareSetPubkey = async () => {
-        const name = dnsEncode("alice.eth");
-
-        const x = ethers.utils.formatBytes32String("foo");
-        const y = ethers.utils.formatBytes32String("bar");
-
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).setPubkey(name, x, y)
-        const rec = await tx.wait();
-    }
-    const prepareSetDNS = async () => {
-        const node = ethers.utils.namehash("alice.eth");
-
-        const record = dnsWireFormat("a.example.com", 3600, 1, 1, "1.2.3.4")
-
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).setDNSRecords(
-            node,
-            "0x" + record
-        )
-        const rec = await tx.wait();
-    }
-    const prepareSetZonehash = async () => {
-        const node = ethers.utils.namehash("alice.eth");
-
-        const record = dnsWireFormat("a.example.com", 3600, 1, 1, "1.2.3.4")
-
-        const tx = await l2PublicResolver.connect(alice.connect(l2Provider)).setZonehash(
-            node,
-            keccak256(toUtf8Bytes("foo"))
-        )
-        const rec = await tx.wait();
-    }
-
-    const prepareTestSubdomain = async () => {
-        const name = dnsEncode("a.b.c.alice.eth");
-        const recordName = "my-slot";
-        const value = "my-subdomain-record";
-
-        await l2PublicResolver.connect(alice.connect(l2Provider)).setText(name, recordName, value);
-    };
-    const prepareTestSubdomain2 = async () => {
-        const name = dnsEncode("alice.eth");
-
-        const recordName = "bobs-slot";
-        const value = "bobs-subdomain-record";
-
-
-
-        await l2PublicResolver.connect(bob.connect(l2Provider)).setText(name, recordName, value);
-    };
-    const nameWrapperProfile = async () => {
-        const name = dnsEncode("namewrapper.alice.eth");
-        const recordName = "namewrapper-slot";
-        const value = "namewrapper-subdomain-record";
-
-        await l2PublicResolver.connect(alice.connect(l2Provider)).setText(name, recordName, value);
-    }
-    //Prepare foreign resolver
-    const prepareForeign = async () => {
-        const name = dnsEncode("alice.eth");
-        await foreignResolver.connect(alice.connect(l2Provider))["setAddr(bytes,address)"](name, alice.address);
-    };
 
     const prepateProofServiceTestContractBytes32 = async () => {
-        await proofServiceTestContract.setBytes32(ethers.utils.namehash("alice.eth"));
         await proofServiceTestContract.setBool(true)
+        await proofServiceTestContract.setAddress(alice.address)
         await proofServiceTestContract.setUint256(123)
+        await proofServiceTestContract.setBytes32(ethers.utils.namehash("alice.eth"));
+        await proofServiceTestContract.setBytes(ethers.utils.namehash("alice.eth"));
+        await proofServiceTestContract.setString("Hello from Alice");
     }
-    await prepareTestPrimitiveSlot();
-    await prepareTestSingleSlot();
-    await prepareTest31yte();
-    await prepeTestMultipleSlots();
-    await prepareSetAddr();
-    await prepareSetAbi();
-    await prepareSetContentHash();
-    await prepareSetName();
-    await prepareSetPubkey();
-    await prepareSetDNS();
-    await prepareSetZonehash();
-    await prepareTestSubdomain();
-    await prepareTestSubdomain2();
-    await nameWrapperProfile();
-    await prepareForeign();
-
     await prepateProofServiceTestContractBytes32();
-
-
     console.log("Environment setup complete wait a few minutes until everything is set");
 };
 
