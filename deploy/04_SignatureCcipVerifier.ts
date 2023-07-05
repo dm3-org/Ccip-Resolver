4d74bfc252b3815992aa68f3e9c928716e233eda
AlexNi245 <alex.plutta@googlemail.com>
2023-07-05 19:51:47 +0200

    lint

--- deploy/04_SignatureCcipVerifier.ts
+++ deploy/04_SignatureCcipVerifier.ts
@@ -1,11 +1,11 @@
 import hre, { ethers } from "hardhat";
 
-const CCIP_Resolver_Address = "0x410ebbabb4471e9c18cc36642f4057812e125e94";
+const CCIP_RESOLVER_ADDRESS = "0x410ebbabb4471e9c18cc36642f4057812e125e94";
 async function main() {
     const [signer] = await ethers.getSigners();
 
     const SignatureVerifier = await ethers.getContractFactory("SignatureCcipVerifier");
-    const deployTx = await SignatureVerifier.deploy(signer.address, CCIP_Resolver_Address, [signer.address]);
+    const deployTx = await SignatureVerifier.deploy(signer.address, CCIP_RESOLVER_ADDRESS, [signer.address]);
     await deployTx.deployed();
 
     console.log(`SignatureCcipVerifier deployed at  ${deployTx.address}`);
