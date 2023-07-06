6ef5be47ad85d71eb6f026c5ea40bf3389cffdac
AlexNi245 <alex.plutta@googlemail.com>
2023-07-06 14:21:36 +0200

    add abi exporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,6 +12,7 @@ import "hardhat-deploy";
 import "hardhat-storage-layout";
 import "hardhat-tracer";
 import "solidity-coverage";
+import 'hardhat-abi-exporter'
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const OPTIMISTIC_ETHERSCAN_API_KEY = process.env.OPTIMISTIC_ETHERSCAN_API_KEY;
@@ -22,10 +23,10 @@ const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY ?? ethers.Wallet.c
 const hardhat = process.env.CI
     ? {}
     : {
-          forking: {
-              url: "http://localhost:8545",
-          },
-      };
+        forking: {
+            url: "http://localhost:8545",
+        },
+    };
 
 module.exports = {
     defaultNetwork: "hardhat",
@@ -62,5 +63,14 @@ module.exports = {
     typechain: {
         outDir: "typechain",
         target: "ethers-v5",
+    },
+    abiExporter: {
+        path: './build/contracts',
+        runOnCompile: true,
+        clear: true,
+        flat: true,
+        except: [],
+        spacing: 2,
+        pretty: true,
     },
 };
