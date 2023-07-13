c45e73d18fb5273a1c1c2c42af4d0d5c2204f444
AlexNi245 <alex.plutta@googlemail.com>
2023-07-13 11:35:58 +0200

    apply decoding for staticccall

--- test/gateway/signatureHandler.test.ts
+++ test/gateway/signatureHandler.test.ts
@@ -103,6 +103,6 @@ describe("Signature Handler", () => {
 
         const resultString = await signatureResolver.resolveWithProof(response.body.data, callData);
 
-        expect(resultString).to.equal(result);
+        expect(ethers.utils.defaultAbiCoder.decode(["bytes"], resultString)[0]).to.equal(result);
     });
 });
