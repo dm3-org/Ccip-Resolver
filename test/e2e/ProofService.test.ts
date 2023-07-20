ce50759bcff3a9cf2c2ae84db476d959af157c15
AlexNi245 <alex.plutta@googlemail.com>
2023-07-20 11:14:51 +0200

    add additional test for long strings

--- test/e2e/ProofService.test.ts
+++ test/e2e/ProofService.test.ts
@@ -84,4 +84,17 @@ describe("ProofServiceTest", () => {
         const responseBytes = await bedrockProofVerifier.getProofValue(proof);
         expect(Buffer.from(responseBytes.slice(2), "hex").toString()).to.equal("Hello from Alice");
     });
+    it("long string slot", async () => {
+        const longString = "Praesent elementum ligula et dolor varius lobortis. Morbi eget eleifend augue. Nunc quis lectus in augue feugiat malesuada vitae sed lacus. Aenean suscipit tristique mauris a blandit. Maecenas ut lectus quis metus commodo tincidunt. Aliquam erat volutpat. Fusce non erat malesuada, consequat mauris id, tincidunt nisi. Aenean a pulvinar ex. Mauris ullamcorper eget odio nec eleifend. Donec pellentesque et tellus id consectetur. Nullam dictum, felis sit amet consectetur convallis, leo lorem rhoncus nulla, eget tincidunt leo erat sit amet urna. Quisque urna turpis, lobortis laoreet nunc at, fermentum ultrices neque.Proin dignissim enim non arcu elementum tempor.Donec convallis turpis at erat luctus, eget pellentesque augue blandit.In faucibus rhoncus mollis.Phasellus malesuada, mauris ut finibus venenatis, ex risus consectetur dolor, quis suscipit augue magna iaculis leo.Proin non nibh at justo porttitor sollicitudin.Pellentesque id malesuada tellus.Aliquam condimentum accumsan ex eu vulputate.Aenean faucibus a quam vitae tincidunt.Fusce vitae mollis nunc, at volutpat ex.Vestibulum sed tellus urna.Donec ac urna lectus.Phasellus a dolor elit.Aenean bibendum hendrerit elit, in cursus sem maximus id.Sed porttitor nulla non consectetur vehicula.Fusce elementum, urna in gravida accumsan, lectus arcu congue augue, at rhoncus purus nunc ac libero."
+
+        const proofService = new ProofService(l1Provider, l2Provider);
+        const { proof } = await proofService.createProof(
+            PROOF_SERVICE_TEST_CONTRACT,
+            "0x0000000000000000000000000000000000000000000000000000000000000006",
+            StorageLayout.DYNAMIC
+        );
+
+        const responseBytes = await bedrockProofVerifier.getProofValue(proof);
+        expect(Buffer.from(responseBytes.slice(2), "hex").toString()).to.equal(longString);
+    });
 });
